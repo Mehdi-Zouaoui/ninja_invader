c4cf8a1db42b9c4abd7cf8ac9007fc1a26dcb36b
Mehdi95740 <mdzouaoui@orange.fr>
2018-10-16 11:44:05 +0200

    OUAIS V 3

--- scripts/index.js
+++ scripts/index.js
@@ -9,12 +9,13 @@ canvas.height = 540;
 
 let timer;
 let player;
-
+let aliens;
 // Chargement de l'image du sprit avant de démarrer le jeu
 const spritesheet = new Image();
 spritesheet.src ="../img/spritesheet.png" ;
 spritesheet.onload = function(){// Fonction exécutée lorsque le navigateur a fini de charger le PNG
-    player = creatPlayer();
+    player = createPlayer();
+    aliens = createAliens();
 
     //Démarrage de la bouche continue
     gameloop();
@@ -23,11 +24,13 @@ spritesheet.onload = function(){// Fonction exécutée lorsque le navigateur a f
 // Gestion mécanisme de jeu
 function update() {
         animatePlayer(); // Fonction qui gère l'animation du joueur
+        animateAliens(); // Fonction qui gère l'animation du Alien
 }
 // Dessin
 function render() {
     context.clearRect(0 , 0 , canvas.width, canvas.height)
     renderPlayer(); // Dessin du joueur
+    renderAliens(); // Dessin de l'alien
 }
 
 // Fonction gérant la boucle de jeu
--- scripts/player.js
+++ scripts/player.js
@@ -1,67 +1,72 @@
-function creatPlayer(){
+const bulletsprite = new Image();
+bulletsprite.src = "../img/shuriken.png";
+
+function createPlayer() {
     // Création d'un objet littéral JS représentant le joueur et ses propriétés 
     const player = {
-           x : 100,
-        y : 450,
-        speed : 6, 
-        lives : 3, 
-        sprite : {
-            img : spritesheet,
-            offsetX : 88,
-            offsetY : 3,
-            width : 26,
-            height : 16
-            
+        x: 100,
+        y: 450,
+        speed: 6,
+        lives: 3,
+        sprite: {
+            img: spritesheet,
+            offsetX: 88,
+            offsetY: 3,
+            width: 26,
+            height: 16
+
         },
-        bullet : null 
+        bullet: null
     };
     // brackets []
     // curly brackets{}
-    return player ;
+    return player;
 }
 
 
-function animatePlayer(){
-    if(Keyboard.RIGHT){
+function animatePlayer() {
+    if (Keyboard.RIGHT) {
         player.x += player.speed;
     }
-    if(Keyboard.LEFT){
+    if (Keyboard.LEFT) {
         player.x -= player.speed;
     }
 
     // Gestion du débordement d'écran du joueur
-    if(player.x < 0){
+    if (player.x < 0) {
         player.x = 0;
-}
-else if ( player.x + player.sprite.width > canvas.width){
-    player.x = canvas.width - player.sprite.width;
-}
-//Si le joueur tire
-if(Keyboard.SPACE){
-    if(player.bullet === null){
-        player.bullet = {
-            x : player.x + player.sprite.width /2 - 2,
-            y : player.y,
-            width : 4,
-            height : 15,
-            color :'rgb(243, 150, 179)',
-            speed : 17
-                }
     }
+    else if (player.x + player.sprite.width > canvas.width) {
+        player.x = canvas.width - player.sprite.width;
+    }
+    //Si le joueur tire
+    if (Keyboard.SPACE) {
+        if (player.bullet === null) {
+            player.bullet = {
+                x: player.x + player.sprite.width / 2 - 2,
+                y: player.y,
+                img: bulletsprite,
+                width: 50,
+                height: 50,
+                speed: 10,
+                rotation: Math.PI * 2
+            };
+        }
 
-}
-// Etat d'avancement du shoot du joueur
-if(player.bullet !== null){
-    player.bullet.y -= player.bullet.speed;
+    }
+    // Etat d'avancement du shoot du joueur
+    if (player.bullet !== null) {
+        player.bullet.y -= player.bullet.speed;
+        //player.bullet.x -= player.bullet.rotation;
 
-    if(player.bullet.y + player.bullet.height < 0){
-        player.bullet = null;
+        if (player.bullet.y + player.bullet.height < 0) {
+            player.bullet = null;
+        }
     }
 }
-}
 
-function renderPlayer(){
-   // Voir si on rentre dans la boucle console.log('ho')
+function renderPlayer() {
+    // Voir si on rentre dans la boucle console.log('ho')
     // Dessin du joueur à ses coordonnées
     context.drawImage(
         player.sprite.img,
@@ -69,6 +74,7 @@ function renderPlayer(){
         player.sprite.offsetY,
         player.sprite.width,
         player.sprite.height,
+
 
         player.x,
         player.y,
@@ -78,14 +84,20 @@ function renderPlayer(){
     );
 
     // Dessin du shoot joueur
-    if(player.bullet !== null){
-    context.fillStyle = player.bullet.color;
-    context.fillRect(
+    if (player.bullet !== null) {
+        // context.fillStyle = player.bullet.color;
+        /** context.save();
+         context.translate( player.bullet.x + player.bullet.width/2 , player.bullet.y + player.bullet.height/2);
+        */
+        context.drawImage(
+            player.bullet.img,
             player.bullet.x,
             player.bullet.y,
             player.bullet.width,
-            player.bullet.height
-    );
+            player.bullet.height,
 
-}
+
+        )
+        // context.restore();
+    }
 }
